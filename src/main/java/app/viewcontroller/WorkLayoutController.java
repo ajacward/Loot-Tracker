d69c218243b91cd10fa7f77c0f136bdfcfd9ec21
ajacward <ajward@unomaha.edu>
2018-03-01 22:19:04 -0600

    implement add loot

--- src/main/java/app/viewcontroller/WorkLayoutController.java
+++ src/main/java/app/viewcontroller/WorkLayoutController.java
@@ -2,12 +2,28 @@ package app.viewcontroller;
 
 import app.MainApp;
 import app.model.Loot;
+import app.model.LootType;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 
 public class WorkLayoutController {
+  @FXML
+  private TextField nameField;
+  @FXML
+  private TextField quantityField;
+  @FXML
+  private TextField goldPieceField;
+  @FXML
+  private TextField notesField;
+  @FXML
+  private Button addButton;
+
   @FXML
   private TableView<Loot> lootTable;
   @FXML
@@ -46,4 +62,43 @@ public class WorkLayoutController {
 
     lootTable.setItems(mainApp.getLootData());
   }
+
+  @FXML
+  private void handleAddLoot() {
+    if (isInputValid()) {
+      Loot tempLoot =
+          new Loot(LootType.WEAPON, nameField.getText(), Integer.parseInt(quantityField.getText()),
+              Double.parseDouble(goldPieceField.getText()), notesField.getText());
+
+      mainApp.getLootData().add(tempLoot);
+    }
+  }
+
+  private boolean isInputValid() {
+    String errorMessage = "";
+
+    if (nameField.getText() == null || nameField.getText().length() == 0) {
+      errorMessage += "No valid loot name!\n";
+    }
+
+    if (quantityField.getText() == null || quantityField.getText().length() == 0) {
+      errorMessage += "No valid quantity!\n";
+    }
+
+    if (errorMessage.length() == 0) {
+      return true;
+    }
+
+    // Show the error message.
+    Alert alert = new Alert(AlertType.ERROR);
+    alert.initOwner(mainApp.getPrimaryStage());
+    alert.setTitle("Invalid Fields");
+    alert.setHeaderText("Please correct invalid fields");
+    alert.setContentText(errorMessage);
+
+    alert.showAndWait();
+
+    return false;
+  }
+
 }
